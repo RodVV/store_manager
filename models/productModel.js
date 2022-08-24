5333d53299c0719a922f82f29b1c3c697c47d9a5
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2022-08-24 16:41:17 -0300

    arquivo productModel, modifica products para StoreManager.products

--- models/productModel.js
+++ models/productModel.js
@@ -1,12 +1,15 @@
 const connection = require('./connection');
 
 const getAllProducts = async () => {
-  const [result] = await connection.execute('SELECT * FROM products;');
+  const [result] = await connection.execute('SELECT * FROM StoreManager.products;');
   return result;
 };
 
 const getProductById = async (id) => {
-  const [result] = await connection.execute('SELECT * FROM products WHERE id =?;', [id]);
+  const [result] = await connection.execute(
+    'SELECT * FROM StoreManager.products WHERE id =?;',
+    [id],
+  );
   if (!result.length) return null;
   return result[0];
 };
