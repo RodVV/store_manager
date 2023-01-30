781101e39c8f6e951dd7fc37d0e5150beb71a2c8
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2023-01-29 23:24:50 -0300

    ultimo commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // const dotenv = require('dotenv');
 const productRoutes = require('./routes/productRoutes');
-// const salesRoutes = require('./routes/salesRoutes');
+const salesRoutes = require('./routes/salesRoutes');
 
 // dotenv.config();
 // console.log(productRoutes);
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/products', productRoutes);
-// app.use('/sales', salesRoutes);
+app.use('/sales', salesRoutes);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
